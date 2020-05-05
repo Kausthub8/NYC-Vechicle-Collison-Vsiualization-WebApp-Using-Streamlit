a0300513fa4882bb63350b1d51dee12f84be44cd
Dude-1999 <kausthub1369@gmail.com>
2020-05-05 20:37:44 +0530

    Add files via upload

---
+++ importing.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+import plotly.express as px
+DATA_URL = (
+"/home/madhuri/Documents/nypd-motor-vehicle-collisions.csv"
+)
